ba61ca46038f451356f292ea946babe8bf27391b
Gunel2727 <gunelemu@code.edu.az>
2026-05-04 23:27:19 +0400

    added

--- WebApplication2/Controllers/OrganizerController.cs
+++ WebApplication2/Controllers/OrganizerController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApplication2.Data;
 
 namespace WebApplication2.Controllers
@@ -9,6 +10,13 @@ namespace WebApplication2.Controllers
     [ApiController]
     public class OrganizerController(ApiAppDbContext apiAppDbContext,IMapper mapper) : ControllerBase
     {
+        public async Task<IActionResult> GetAllEvents()
+        {
+            var events = await apiAppDbContext.Organizers
+                 .Include(e => e.Events)
+                 .ToListAsync();
+            return Ok(events);
+        }
 
     }
 }
--- WebApplication2/obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b29b0930875186d24d142dfd318e452f46cc5370")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
